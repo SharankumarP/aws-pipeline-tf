ea4e319612afd46a930c03852ff29d44d07f94c0
SharankumarP <kandulasharankumar12@gmail.com>
2024-05-21 11:31:45 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   type    = string
   default = "eu-central-1"
 }
-variable "s3_test_bucket_name" {
+variable "s3_bucket_name" {
   type    = string
   default = "tfbackend-bucket"
 }
