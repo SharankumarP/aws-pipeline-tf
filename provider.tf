1706fdbc63cc6edc0b9d8b23317f005a2c0d7e18
SharankumarP <kandulasharankumar12@gmail.com>
2024-05-21 09:31:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,10 @@ terraform {
       version = "~> 4.0"
     }
   }
-}
+  backend "s3"{
+    bucket = "video-tf-backend"
+    key    = "backend"
+    region = "eu-west-2a"
 
 provider "aws" {
   region = "eu-west-2a"
