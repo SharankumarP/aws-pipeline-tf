9069c7adc54a9cfb1526deaea38d31e3e66bd201
SharankumarP <kandulasharankumar12@gmail.com>
2024-05-21 11:37:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "s3_test_bucket_backend" {
+resource "aws_s3_bucket" "s3_test8_bucket_backend" {
   #checkov:skip=CKV_AWS_145: Lifecycle configuration not required for TF state bucket
   #checkov:skip=CKV2_AWS_61: Lifecycle configuration not required for TF state bucket
   #checkov:skip=CKV_AWS_144: Cross-region replication not required for TF state bucket
@@ -11,31 +11,31 @@ resource "aws_s3_bucket" "s3_test_bucket_backend" {
 
 resource "aws_s3_bucket_acl" "s3_bucket_backend_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
-  bucket     = aws_s3_bucket.s3_test_bucket_backend.id
+  bucket     = aws_s3_bucket.s3_test8_bucket_backend.id
   acl        = "private"
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = aws_s3_bucket.s3_test_bucket_backend.id
+  bucket = aws_s3_bucket.s3_test8_bucket_backend.id
   rule {
     object_ownership = "ObjectWriter"
   }
 }
 
 resource "aws_s3_bucket_versioning" "s3_bucket_version" {
-  bucket = aws_s3_bucket.s3_test_bucket_backend.id
+  bucket = aws_s3_bucket.s3_test8_bucket_backend.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_object" "terraform_folder" {
-  bucket = aws_s3_bucket.s3_test_bucket_backend.id
+  bucket = aws_s3_bucket.s3_test8_bucket_backend.id
   key    = "terraform.tfstate"
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
-  bucket                  = aws_s3_bucket.s3_test_bucket_backend.bucket
+  bucket                  = aws_s3_bucket.s3_test8_bucket_backend.bucket
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
