8412ebcd3f78d8a409e4fd6bbbe121a1492ca9d3
SharankumarP <kandulasharankumar12@gmail.com>
2024-05-21 11:29:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,32 +9,32 @@ resource "aws_s3_bucket" "s3_test_bucket_backend" {
   bucket = var.s3_bucket_name
 }
 
-resource "aws_s3_bucket_acl" "s3_test_bucket_backend_acl" {
+resource "aws_s3_bucket_acl" "s3_bucket_backend_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
   bucket     = aws_s3_bucket.s3_bucket_backend.id
   acl        = "private"
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3_test_bucket_acl_ownership" {
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.s3_bucket_backend.id
   rule {
     object_ownership = "ObjectWriter"
   }
 }
 
-resource "aws_s3_bucket_versioning" "s3_test_bucket_version" {
+resource "aws_s3_bucket_versioning" "s3_bucket_version" {
   bucket = aws_s3_bucket.s3_bucket_backend.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_test_object" "terraform_folder" {
+resource "aws_s3_object" "terraform_folder" {
   bucket = aws_s3_bucket.s3_bucket_backend.id
   key    = "terraform.tfstate"
 }
 
-resource "aws_s3_bucket_public_access_block" "s3_test_bucket_access" {
+resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
   bucket                  = aws_s3_bucket.s3_bucket_backend.bucket
   block_public_acls       = true
   block_public_policy     = true
